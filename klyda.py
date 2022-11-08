ec1e631a1aeac18dbea4f9ed79b94f8e2b8c1cb1
Xeonrx <99039115+Xeonrx@users.noreply.github.com>
2022-11-08 14:39:41 -0500

    Update klyda.py

--- klyda.py
+++ klyda.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
      sys.path.append(f'{os.path.abspath(os.getcwd())}/klyda');import main
      main.Klyda()
 
-# Version v1.1, leaded by Xeonrx
+# Version v1.0.1, leaded by Xeonrx
